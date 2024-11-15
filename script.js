539a2616a5df03be428a12396cf0a9db4a6c6e25
aiuyap <186591613+aiuyap@users.noreply.github.com>
2024-11-15 20:21:45 +0800

    Created constructor and function to add new books in the array

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(author, title, pages, readStatus) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.readStatus = readStatus;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(author, title, pages, readStatus) {
+    const book = new Book(author, title, pages, readStatus);
+    myLibrary.push(book);
 }
+
+addBookToLibrary("Aiu", "Pogi", 2, true);
+addBookToLibrary("Irine", "Gwapa", 2, true);
+
+console.log(myLibrary);
